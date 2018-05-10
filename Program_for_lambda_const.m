60df5f7f44df15efad2a887fe4370d2e2dd13781
MikeKurshakov <kurshakov.mihail@gmail.com>
2018-05-10 11:38:00 +0300

    :skull: New stop creteria add

--- Program_for_lambda_const.m
+++ Program_for_lambda_const.m
@@ -1,16 +1,16 @@
 func=@(x)(10*(x(1)-x(2))^2+(x(1)-1)^2)^(4)
 %func=@(x)x(1)^2+25*x(2)^2
-eps=0.0001
+eps=0.01
 x0=[-1.2;0]
 x_k=x0
 counter=0
 lambdaCONST=1
 
 while(1)
-    if(norm(Gradient(func,x_k))<eps)
-        x_res=x_k
-        break
-    end 
+%     if(norm(Gradient(func,x_k))<eps)
+%         x_res=x_k
+%         break
+%     end 
     x_new=x_k-lambdaCONST.*Gradient(func,x_k)./norm(Gradient(func,x_k))
     if(func(x_new)-func(x_k)<0)
         if(norm(x_new-x_k)<eps)
