25c7bbfd5af8415f500cc16aea8291d6c5325202
MikeKurshakov <kurshakov.mihail@gmail.com>
2018-05-10 11:36:17 +0300

    Count gradient :shit:

--- Gradient.m
+++ Gradient.m
@@ -1,5 +1,5 @@
 function grad=Gradient(func,x0)
-h = 0.0000000001
+h = 0.00000001
 %x1=(-func([x0(1)+2.*h x0(2)])+8.*func([x0(1)+h x0(2)])-8.*func([x0(1)-h x0(2)])+func([x0(1)-2.*h x0(2)]))./(12.*h)
 %x2=(-func([x0(1) x0(2)+2.*h])+8.*func([x0(1) x0(2)+h])-8.*func([x0(1) x0(2)-h])+func([x0(1) x0(2)-2.*h]))./(12.*h)
 x1=(func([x0(1)+h x0(2)])-func([x0(1) x0(2)]))/h
